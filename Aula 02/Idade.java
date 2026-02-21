cea73b16015f9669e76afcd0525e68d6e386bc5f
aluno <aluno0@unicap.br>
2026-02-20 21:14:36 -0300

    Create Idade.java

---
+++ Aula 02/Idade.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Idade {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Digite a sua idade: ");
+        int idade = input.nextInt();
+        
+        if (idade >= 18) {
+            System.out.println("Você já pode tirar carteira de motorista!");
+        } else {
+            System.out.println("Você ainda não pode tirar carteira de motorista!");
+        }
+        if (idade < 16) {
+            System.out.println("Você não pode votar");
+        } else if (idade < 18){
+            System.out.println("Você pode tirar o titulo de eleitor e votar!");
+        } else if (idade < 70){
+            System.out.println("É obrigatorio ter título de eleitor e votar!");
+        } else {
+            System.out.println("Você não tem mais obrigação de votar!");
+        }
+
+        input.close();
+    }
+}
